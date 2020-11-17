a34cbdfee425076fd01e3c1f0472b151765d5011
Yury Rivera <yrivera@applaudostudios.com>
2020-11-17 15:23:24 -0600

    :white_check_mark: updating test imports

--- src/test/transformers/mutable-transformer.test.ts
+++ src/test/transformers/mutable-transformer.test.ts
@@ -1,11 +1,12 @@
-import { mutableTransformer } from '../transformers/mutable-transformer';
-import { createSnakeCaseObject, createCamelCaseObject } from '../utils/test-data';
+import { mutableTransformer } from '../../transformers/mutable-transformer';
+import { toCamelCase } from '../../cases/camel-case';
+import { createSnakeCaseObject, createCamelCaseObject } from '../../utils/test-data';
 
 
 describe('Mutable transfor test', () => {
-    it('should return snake object as camel case', () => {
-        const test = createSnakeCaseObject(2);
-        const result = mutableTransformer(test);
-        expect(result).toEqual(createCamelCaseObject(2));
-    });
+  it('should return snake object as camel case', () => {
+    const test = createSnakeCaseObject(2);
+    const result = mutableTransformer(test, toCamelCase);
+    expect(result).toEqual(createCamelCaseObject(2));
+  });
 });
